e0b22ae31a7be96490a1d9c4ba56ca9c8cf7a78f
sendmebits <cgreco@gmail.com>
2024-06-10 15:57:12 -0400

    Update Footer.tsx
    simplifying

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,17 +28,6 @@ export default function Footer() {
         <Box
             bg={useColorModeValue('gray.50', 'gray.900')}
             color={useColorModeValue('gray.700', 'gray.200')} mt={'28'}>
-            <Container as={Stack} maxW={'6xl'} pt={'16'} pb={'32'}>
-                <SimpleGrid
-                    templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr' }}
-                    spacing={8}>
-                    <Stack spacing={2}>
-                        <Text fontSize={'sm'}>
-                            Minimalistic disposable email management
-                        </Text>
-                    </Stack>
-                </SimpleGrid>
-            </Container>
             <Center py={10} color={'gray.600'}>
                 cfalias is not affiliated with Cloudflare
             </Center>
